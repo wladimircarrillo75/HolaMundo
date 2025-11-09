95cc1085eab7460041592aee14e1140575a7536e
wladimircarrillo75 <wladimircarrillo75@gmail.com>
2025-11-09 17:38:48 -0500

    Commit 2

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -14,7 +14,7 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Prueba con Git hub");
     }
     
 }
